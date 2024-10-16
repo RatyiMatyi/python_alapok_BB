5638f47612cd75063ccb5e496eeb5b6b0c829717
Barta Bulcsú <barta.bulcsu@diak.szbi-pg.hu>
2024-10-16 13:59:33 +0200

    New file

---
+++ 10_14/07_For_ciklus.py
@@ -0,0 +1,117 @@
+"""Fájlnév:
+07_0_for_ciklus_feladatok
+
+&quot;&quot;&quot;0. Feladat: Számok kiírása
+Írj egy programot, amely kiírja az 1-től 20-ig a számokat!
+1
+2
+3
+...
+&quot;&quot;&quot;
+
+
+&quot;&quot;&quot;
+
+...&quot;&quot;&quot;
+&quot;&quot;&quot;2. Feladat: Számok összege
+
+
+&quot;&quot;&quot;2.b Feladat: Számok összege
+
+Az összeg: __&quot;&quot;&quot;
+&quot;&quot;&quot;3. Feladat: Szorzótábla
+
+
+&quot;&quot;&quot;4. Feladat: Számok visszafelé
+
+8&quot;&quot;&quot;
+&quot;&quot;&quot;5. Feladat: Csillagok kirajzolása
+
+*
+**
+***&quot;&quot;&quot;
+&quot;&quot;&quot;6. Feladat: Számok négyzetei
+ 4&quot;&quot;&quot;"""
+
+
+"""for i in range(1, 21):
+    (print)(i)"""
+
+#"""&quot);&quot;&quot;0.b Feladat: Számok kiírása
+#Írj egy programot, amely kiírja az 1-től 20-ig a számokat a minta alapján!
+#1 2 3 4 ..."""
+
+"""for i in range(1,21):
+    print(i, end=" ")"""
+
+
+"""&quot;&quot;&quot;1. Feladat: Páros számok kiírása
+
+"""
+
+"""for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)"""
+
+
+"""Készíts egy programot, amely kiszámítja és kiírja az 1 és 10 közötti számok
+összegét!
+Az összeg: __
+&quot;&quot;&quot;"""
+
+"""osszeg = 0
+
+for i in range(1, 11):
+    osszeg += i
+print(f"Az összeg: {osszeg}" )"""
+
+"""Készíts egy programot, amely kiszámítja és kiírja az 1 és 100 közötti számok
+összegét!"""
+
+"""osszeg = 0
+for i in range(1, 101):
+    osszeg = osszeg + i
+print(osszeg)"""
+
+
+"""Készíts egy programot, amely kiírja a 7-es szorzótáblát 1-től 10-ig!
+7 x 1 = 7
+7 x 2 = 14&quot;&quot;&quot;"""
+
+"""for i in range(1, 11):
+    osszeg = 7 * i
+    print(f"7x {i}= {osszeg}")"""
+
+
+"""Írj egy programot, amely kiírja a számokat 10-től 1-ig csökkenő sorrendben!
+10
+9"""
+
+"""for i in range(1, 11):
+    print(11 - i)"""
+
+"""Készíts egy programot, amely egy háromszöget rajzol csillagokból! Az első sorban 1
+csillag, a másodikban 2, és így tovább, összesen 5 sorban."""
+
+"""for i in range(6):
+    print("x " * i)
+    i =+ 1"""
+
+"""Írj egy programot, amely kiírja az 1 és 20 közötti páros számokat!
+2
+4
+6"""
+
+"""for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)"""
+
+
+"""Írj egy programot, amely kiírja az 1-től 10-ig terjedő számok négyzeteit!
+1 négyzete: 1
+2 négyzete:"""
+
+"""for i in range(1, 11):
+    negyzet = i * i
+    print(f"{i} négyzete= {negyzet}")"""
+
